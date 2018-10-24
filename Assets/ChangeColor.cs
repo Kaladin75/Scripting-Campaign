6d49ce92f08b56697fe54da6644656a2350f67a5
Kaladin75 <dylankennedy175@gmail.com>
2018-10-24 09:39:08 -0700

    How to change game object color and blink light

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		}
+
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+		if (Input.GetKeyDown (KeyCode.B)) 
+		{
+			gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+		}
+	}
+}
---
+++ Assets/VariablesAndFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+	int myInt = 5;
+	string myName = "Dylan Kennedy";
+
+	void Start(){
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+		printTextConsole ("hjoasdvjoaehrvgf");
+ 	}
+
+	int MultiplyByTwo(int number){
+		int ret;
+
+		ret = number * 2;
+
+		return ret;
+
+
+	}
+
+	void printTextConsole (string textToPrint){
+		Debug.Log (textToPrint);
+	}
+
+
+
+
+
+ }
